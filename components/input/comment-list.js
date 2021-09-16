67a175956c0aec0b954f8fee7a1432dc4745d8ff
Taeyang Kim <mynamesunpower@gmail.com>
2021-09-16 16:35:38 +0900

    Comments Components

---
+++ components/input/comment-list.js
@@ -0,0 +1,20 @@
+import classes from "./comment-list.module.css";
+
+function CommentList({ comments }) {
+  return (
+    <ul className={classes.comments}>
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <p>{comment.text}</p>
+            <div>
+              By <address>{comment.name}</address>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default CommentList;
